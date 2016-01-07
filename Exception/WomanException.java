e9d67f688d6b00cfbe8f2b752ebc408389f7a690
kellymr1 <kellymr1@rose-hulman.edu>
2016-01-07 09:37:32 -0600

    Create WomanException.java

---
+++ Exception/WomanException.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+
+/**
+ * @author kellymr1
+ *
+ */
+public class WomanException extends Exception {
+
+	/**
+	 * 
+	 */
+	public WomanException() {
+		// TODO Auto-generated constructor stub
+		super("I can't believe you would say that to me!");
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public WomanException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public WomanException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public WomanException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 * @param arg2
+	 * @param arg3
+	 */
+	public WomanException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
